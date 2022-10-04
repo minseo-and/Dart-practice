2de4f3cb00cbe3d27f03688479f0304ea3cea82e
minseo-and <msp05070478@gmail.com>
2022-10-04 17:04:02 +0900

    while

---
+++ src/While.dart
@@ -0,0 +1,15 @@
+void main() {
+  int total = 0;
+
+  while(total<20) {
+    print(total);
+    total ++;
+  };
+  total = 0;
+  do {
+    print(total);
+    total ++;
+  } while(total<20);
+  total = 0;
+
+}
