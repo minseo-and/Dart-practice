7150f771e2ec93e13a4abe0d6a9dc2325d013751
minseo-and <msp05070478@gmail.com>
2022-10-04 14:40:22 +0900

    list

---
+++ src/List.dart
@@ -0,0 +1,18 @@
+void main() {
+  List nameList = [
+    'John',
+    'Mike',
+    'Jane',
+  ];
+
+  List<String> sportsList = [
+    'soccer',
+    'baseball',
+    'basketball'
+  ];
+
+  print(nameList);
+  print(sportsList);
+  print(nameList[0]);
+  print(sportsList[1]);
+}
