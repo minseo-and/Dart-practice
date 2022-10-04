4ecf1d07dfe892263a84c27389094790d8339143
minseo-and <msp05070478@gmail.com>
2022-10-04 16:57:08 +0900

    if

--- src/If.dart
+++ src/If.dart
@@ -8,4 +8,16 @@ void main() {
   } else {
     print('어떤 배수인지 알 수 없습니다.');
   }
+
+  switch(number %3) {
+    case 1:
+      print('나머지가 1입니다.');
+      break;
+    case 2:
+      print('나머지가 2입니다.');
+      break;
+    default:
+      print('어떤 조건에도 해당되지 않습니다.');
+      break;
+  }
 }
