7b3e290d4d2bfc7e4351db0e06b487932a33989d
minseo-and <msp05070478@gmail.com>
2022-10-04 16:55:18 +0900

    if

---
+++ src/If.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = 2;
+
+  if(number % 3 == 0) {
+    print('3의 배수입니다.');
+  } else if(number % 4 == 0){
+    print('4의 배수입니다.');
+  } else {
+    print('어떤 배수인지 알 수 없습니다.');
+  }
+}
