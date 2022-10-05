8c872aa0f12127c2ec1d5bc63e30719ef448f127
minseo-and <msp05070478@gmail.com>
2022-10-05 20:12:31 +0900

    class

--- src/Class.dart
+++ src/Class.dart
@@ -1,18 +1,25 @@
 void main() {
-  Game name = new Game('오버워치');
+  Game name = new Game('맥크리', '오버워치');
   name.sayGame();
+
+  Game name2 = new Game('캐서디', '오버워치2');
+  name2 .sayGame();
+
 
 }
 
 class Game{
   final name;
+  final group;
 
   Game(
       String name,
-      ) : this.name = name;
+      String group
+      ) : this.name = name,
+      this.group = group;
 
   void sayGame() {
-    print('이 게임은 ${this.name}입니다.');
+    print('이 게임은 ${this.group}이고, 캐릭터는 ${this.name}입니다.');
   }
 
 }
